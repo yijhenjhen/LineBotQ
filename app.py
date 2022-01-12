40d92f76ae3870cc0017afea4ce7729ad6515a55
Katherine <91734733+yijhenjhen@users.noreply.github.com>
2022-01-12 19:11:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -171,16 +171,19 @@ def sendTextMessageToMe():
 
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    productId = "5ac21a8c040ab15980c9b43f"
-    name = ""
+    productId = "5ac21b4f031a6752fb806d59"
+    name = "Katherine"
     message = dict()
     message["type"] = "text"
-    message["text"] = {}
+    message["text"] = "".join("$" for r in range(len(name)))
     emojis_list = list()
     for i, nChar in enumerate(name):
         emojis_list.append(
             {
 
+              "index": i,
+              "productId": productId,
+              "emojiId": f"{lookUpStr.index(nChar) + 1 :03}"
             }
         )
     message["emojis"] = emojis_list
